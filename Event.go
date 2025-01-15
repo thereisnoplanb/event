a710422296c5a2e9ffd093a218475279af347cd3
golangframework <server.ozga@gmail.com>
2025-01-15 19:25:15 +0100

    Without interface.

--- Event.go
+++ Event.go
@@ -2,7 +2,7 @@ package event
 
 import "sync"
 
-type event[TSender any, TEventArgs any] struct {
+type Event[TSender any, TEventArgs any] struct {
 	delegates map[uint32]EventHandler[TSender, TEventArgs]
 	index     uint32
 	mutex     *sync.Mutex
@@ -12,42 +12,13 @@ type Handler struct {
 	index uint32
 }
 
-type Event[TSender any, TEventArgs any] interface {
-	// Adds delegate function to event.
-	//
-	// Parameters:
-	//
-	//	delegate func(sender *TSender, eventArgs TEventArgs) - Delegate function to add. If delegate is nil it is not added to event.
-	//
-	// Returns:
-	//
-	//	handler *Handler - Returns handler to added delegate. If added delegate is nil, nil is returned.
-	Add(delegate func(sender *TSender, eventArgs TEventArgs)) (handler *Handler)
-
-	// Removes delegate function from event.
-	//
-	// Parameters:
-	//
-	//	handler *Handler - Handler to delegate, received in Add(delegate func(sender *TSender, eventArgs TEventArgs)) method.
-	Remove(handler *Handler)
-
-	// Invokes event.
-	// Every added delegate is invoked in indeterminate order, not necessarily in the order of addition.
-	//
-	// Parameters:
-	//
-	//	sender *TSender - Object that sends event.
-	//	eventArgs TEventArgs - Arguments of event.
-	Invoke(sender *TSender, eventArgs TEventArgs)
-}
-
 // Creates a new Event instance.
 //
 // Returns:
 //
 //	Event[TSender, TEventArgs] - A new Event instance.
 func New[TSender any, TEventArgs any]() Event[TSender, TEventArgs] {
-	return &event[TSender, TEventArgs]{
+	return Event[TSender, TEventArgs]{
 		delegates: make(map[uint32]EventHandler[TSender, TEventArgs]),
 		index:     0,
 		mutex:     &sync.Mutex{},
@@ -63,7 +34,7 @@ func New[TSender any, TEventArgs any]() Event[TSender, TEventArgs] {
 // Returns:
 //
 //	handler *Handler - Returns handler to added delegate. If added delegate is nil, nil is returned.
-func (event *event[TSender, TEventArgs]) Add(delegate func(sender *TSender, eventArgs TEventArgs)) (handler *Handler) {
+func (event *Event[TSender, TEventArgs]) Add(delegate func(sender *TSender, eventArgs TEventArgs)) (handler *Handler) {
 	if event != nil && delegate != nil {
 		event.mutex.Lock()
 		defer event.mutex.Unlock()
@@ -81,7 +52,7 @@ func (event *event[TSender, TEventArgs]) Add(delegate func(sender *TSender, even
 // Parameters:
 //
 //	handler *Handler - Handler to delegate, received in Add(delegate func(sender *TSender, eventArgs TEventArgs)) method.
-func (event *event[TSender, TEventArgs]) Remove(handler *Handler) {
+func (event *Event[TSender, TEventArgs]) Remove(handler *Handler) {
 	if event != nil && handler != nil {
 		event.mutex.Lock()
 		defer event.mutex.Unlock()
@@ -97,7 +68,7 @@ func (event *event[TSender, TEventArgs]) Remove(handler *Handler) {
 //
 //	sender *TSender - Object that sends event.
 //	eventArgs TEventArgs - Arguments of event.
-func (event *event[TSender, TEventArgs]) Invoke(sender *TSender, eventArgs TEventArgs) {
+func (event *Event[TSender, TEventArgs]) Invoke(sender *TSender, eventArgs TEventArgs) {
 	if event != nil {
 		event.mutex.Lock()
 		defer event.mutex.Unlock()
